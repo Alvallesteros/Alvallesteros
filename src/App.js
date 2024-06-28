3432987bf1856ffc5be9ea54b3f12b5a599dc218
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2024-06-28 09:59:02 +0800

    added Grid Overlay

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar.js';
 import HeroBanner from './components/hero-banner.js';
+import Grid from './components/gridOverlay.js';
 
 function App() {
   return (
     <div className="App">
+      <Grid />
       <Navbar />
       <HeroBanner />
     </div>
---
+++ src/components/gridOverlay.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from '../styles/grid.module.css';
+
+function Grid() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.margin}></div>
+            <div className={styles.column}></div>
+            <div className={styles.gutter}></div>
+            <div className={styles.column}></div>
+            <div className={styles.gutter}></div>
+            <div className={styles.column}></div>
+            <div className={styles.gutter}></div>
+            <div className={styles.column}></div>
+            <div className={styles.gutter}></div>
+            <div className={styles.column}></div>
+            <div className={styles.gutter}></div>
+            <div className={styles.column}></div>
+            <div className={styles.gutter}></div>
+            <div className={styles.column}></div>
+            <div className={styles.gutter}></div>
+            <div className={styles.column}></div>
+            <div className={styles.gutter}></div>
+            <div className={styles.column}></div>
+            <div className={styles.gutter}></div>
+            <div className={styles.column}></div>
+            <div className={styles.gutter}></div>
+            <div className={styles.column}></div>
+            <div className={styles.gutter}></div>
+            <div className={styles.column}></div>
+            <div className={styles.margin}></div>
+        </div>
+    )
+}
+
+export default Grid;
