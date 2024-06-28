722aa9b000a066096c0cceb726587ae857cff157
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2024-06-28 14:05:00 +0800

    about me finished

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -8,9 +8,14 @@ function AboutMe() {
         <div className={styles.titleContainer}>
             <div className={common.titleText} style={{
                 textAlign: "right",
-            }}><span>A</span>bout Me</div>
+            }}>
+                <span>A</span>bout Me
+            </div>
         </div>
         <div className={styles.underline}></div>
+        <div className={styles.textContainer}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel lorem auctor, malesuada risus eget, laoreet arcu. Mauris id neque in odio mollis scelerisque. Duis iaculis posuere vulputate. Aliquam pulvinar lectus eget dapibus posuere. Nam efficitur, nulla quis molestie laoreet, dolor elit accumsan tortor, ut vulputate lectus purus id turpis. Curabitur eget velit et diam consectetur pharetra. Cras commodo purus ac sem scelerisque imperdiet.
+        </div>
         <div className={styles.background}>
             <div className={styles.gradient}></div>
             <div className={styles.gradient} style={{transform: "rotate(180deg)"}}></div>
