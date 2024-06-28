3ccba03d597c2c278d37773fbf99e71ba0c58954
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2024-06-28 13:58:15 +0800

    about me title added

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import styles from '../styles/about-me.module.css';
+import common from '../styles/common.module.css';
 
 function AboutMe() {
     return (
       <div className={styles.container}>
+        <div className={styles.titleContainer}>
+            <div className={common.titleText} style={{
+                textAlign: "right",
+            }}><span>A</span>bout Me</div>
+        </div>
+        <div className={styles.underline}></div>
         <div className={styles.background}>
             <div className={styles.gradient}></div>
             <div className={styles.gradient} style={{transform: "rotate(180deg)"}}></div>
