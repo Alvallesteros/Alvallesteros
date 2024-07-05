88fdd9a10ae5e99d3e0955443ff9c735a259e4c7
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2024-07-05 15:20:30 +0800

    added burger

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import logo from '../imgs/logo.svg'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { Divide as Hamburger } from 'hamburger-react'
+
+const BurgerMenu = () => {
+    return (
+        <div className='row fill-width fsi-6 burger'>
+            <div className='col-12 col-lg-12 col-md-8 col-s-4 col-xs-4 burger-page p-5'>
+                Home
+            </div>
+            <div className='col-12 col-lg-12 col-md-8 col-s-4 col-xs-4 burger-page p-5'>
+                Projects
+            </div>
+            <div className='col-12 col-lg-12 col-md-8 col-s-4 col-xs-4 burger-page p-5'>
+                About
+            </div>
+        </div>
+    );
+}
+
 
 function Navbar() {
+    const [isOpen, setOpen] = useState(false)
     const [isDesktop, setDesktop] = useState(window.innerWidth > 1023);
 
     const updateMedia = () => {
@@ -32,15 +49,19 @@ function Navbar() {
                         <div className={'col-2 col-lg-2 center navpage'}>
                             About
                         </div>
-                        <div className={"col-4 offset-2 col-lg-5 offset-lg-1 search-bar"}>
+                        {/*<div className={"col-4 offset-2 col-lg-5 offset-lg-1 search-bar"}>
                             <input type='text' placeholder='Search...'/>
-                        </div>
+                        </div>*/}
                     </div>
                 ) : (
                     <div className={'col-md-1 offset-md-6 col-s-1 offset-s-2 col-xs-1 offset-xs-2 justify-end burger-container'}>
-                        <FontAwesomeIcon icon={faBars} className={'hamburger'}/>
+                        <Hamburger color='#FFD540' toggled={isOpen} toggle={setOpen}/>
                     </div>
                 )}
+            </div>
+            <div className={'row fold-content ' + (isOpen ? 'open': null)}>
+                <BurgerMenu />
+                <div className='p-3'></div>
             </div>
         </div>
     );
