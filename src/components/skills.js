563337815f16cdc52b5abc33ae182011bcc24651
Alvallesteros <angelo.alvarez@obf.ateneo.edu>
2024-07-05 13:01:02 +0800

    fixed about

--- src/components/skills.js
+++ src/components/skills.js
@@ -20,15 +20,15 @@ function Skills() {
         <div className='container' style={{padding: '2rem 0', position: 'relative', margin: '0rem'}}>
             <div className='row ycenter'>
                 { isTablet | isDesktop  ? <div className='line'></div> :null }
-                <div className='col-s-3 col-md-6 col-l-8 col-12 center unselectable'>
+                <div className='col-xs-4 col-s-4 col-md-8 col-lg-11 col-12 center unselectable'>
                     <div className='header-3 title-design'>
                         <span>S</span>kills
                     </div>
                 </div>
             </div>
-            <div className='col-10 col-l-6 offset-1 offset-l-1'>
-            <div className='row' style={{gap: "1rem", justifyContent: 'center'}}>
-                    <div className='col-s-3  center item-container'>
+            <div className='col-10 col-lg-10 col-md-6 offset-1 offset-lg-1 col-xs-4 offset-md-1'>
+                <div className='row' style={{gap: "1rem", justifyContent: 'center'}}>
+                    <div className='col-xs-4 center item-container'>
                         <div className='image-container'>
                             <FontAwesomeIcon icon={faPython} className='icon'/>
                         </div>
@@ -36,7 +36,7 @@ function Skills() {
                             <span>Python</span>
                         </div>
                     </div>
-                    <div className='col-s-3 center item-container'>
+                    <div className='col-xs-4 center item-container'>
                         <div className='image-container'>
                             <FontAwesomeIcon icon={faJava} className='icon'/>
                         </div>
@@ -44,7 +44,7 @@ function Skills() {
                             <span>Java</span>
                         </div>
                     </div>
-                    <div className='col-s-3 center item-container'>
+                    <div className='col-xs-4 center item-container'>
                         <div className='image-container'>
                             <FontAwesomeIcon icon={faWindows} className='icon'/>
                         </div>
@@ -52,7 +52,7 @@ function Skills() {
                             <span>Azure</span>
                         </div>
                     </div>
-                    <div className='col-s-3 center item-container'>
+                    <div className='col-xs-4 center item-container'>
                         <div className='image-container'>
                             <FontAwesomeIcon icon={faGitAlt} className='icon'/>
                         </div>
@@ -60,7 +60,7 @@ function Skills() {
                             <span>Git Source Control</span>
                         </div>
                     </div>
-                    <div className='col-s-3 center item-container'>
+                    <div className='col-xs-4 center item-container'>
                         <div className='image-container'>
                             <FontAwesomeIcon icon={faJs} className='icon'/>
                         </div>
@@ -68,7 +68,7 @@ function Skills() {
                             <span>JavaScript</span>
                         </div>
                     </div>
-                    <div className='col-s-3 center item-container'>
+                    <div className='col-xs-4 center item-container'>
                         <div className='image-container'>
                             <FontAwesomeIcon icon={faHtml5} className='icon'/>
                         </div>
